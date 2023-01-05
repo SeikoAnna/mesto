3e10d99b7e641e29bd172fdd405ffc133383ee3c
Anna.Seiko <seiko_anna@inbox.ru>
2023-01-05 19:00:59 +0300

    added nojekyll

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,32 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
-const popupName = document.querySelector('.popup__form-name');
-const popupProfession = document.querySelector('.popup__form-profession');
+const popupName = document.querySelector('.popup__input_type_name');
+const popupProfession = document.querySelector('.popup__input_type_profession');
 const popupSave = document.querySelector('.popup__save');
 const profName = document.querySelector('.profile__name');
 const profProfession = document.querySelector('.profile__profession');
 const popupForm = document.querySelector('.popup__form');
 
-editButton.addEventListener ('click', function() {
+
+editButton.addEventListener ('click', openPopup)
+    function openPopup() {
     popup.classList.add('popup_opened');
     popupName.value=profName.textContent;
     popupProfession.value=profProfession.textContent;
-})
+}
 
-popupClose.addEventListener ('click', function() {
+popupClose.addEventListener ('click', closePopup)
+    function closePopup() {
     popup.classList.remove('popup_opened');
-})
+}
 
-popupForm.addEventListener ('submit', function(event){
+popupForm.addEventListener ('submit', formPopup)
+    function formPopup (event){
     event.preventDefault();
     profName.textContent=popupName.value;
     profProfession.textContent=popupProfession.value;
     popup.classList.remove('popup_opened');
-})
-
-
+}
 
 
