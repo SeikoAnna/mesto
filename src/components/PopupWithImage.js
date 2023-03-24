3e21ed5d49d9173aa1bcda0fbf3359ed8bed7f0f
Anna.Seiko <seiko_anna@inbox.ru>
2023-03-24 19:20:07 +0300

    sprint 9.3

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,11 +3,8 @@ import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
   constructor(popupElement) {
     super(popupElement);
-    this._popupImageBigItem =
-      this._popupElement.querySelector(".popup__image");
-    this._popupImageBigName = this._popupElement.querySelector(
-      ".popup__photo-title"
-    );
+    this._popupImageBigItem = this._popupElement.querySelector(".popup__image");
+    this._popupImageBigName = this._popupElement.querySelector(".popup__photo-title");
   }
 
   open(name, link) {
